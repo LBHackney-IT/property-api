0ab69c1e26a74d1407b91e11cb8bd4d49a757705
Daniele <daniele.tortora@hackney.gov.uk>
2019-03-18 13:14:30 +0000

    setting property getaway

---
+++ property-api/V1/Gateways/PropertyGateway.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using property_api.V1.Infrastructure;
+using property_api.V1.Domain;
+    
+namespace property_api.V1.Gateways
+{
+    
+    public class PropertyGateway : IPropertyGateway 
+    {
+        private readonly IUHContext _uhcontext;
+        public PropertyGateway(IUHContext uhContext)
+        {
+            uhContext = _uhcontext;
+        }
+        public Property GetPropertyByPropertyReference()
+        {
+            return null;
+        }
+    }
+}
